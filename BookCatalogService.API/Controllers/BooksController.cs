3c43d89238702a7dac2301e02eb2486f3c20a703
RajeshVattam45 <>
2025-11-30 00:06:54 +0530

    removed space

--- BookCatalogService.API/Controllers/BooksController.cs
+++ BookCatalogService.API/Controllers/BooksController.cs
@@ -4,7 +4,6 @@ namespace BookCatalogService.API.Controllers
 {
     public class BooksController : Controller
     {
-       
         public IActionResult Index ( )
         {
             return View ();
